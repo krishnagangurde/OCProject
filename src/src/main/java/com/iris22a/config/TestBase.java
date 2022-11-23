59216957b5d1e3e2f8fff3ef2a23c407ad8b9daa
krushna@10 <krushnagangurde10@gmail.com>
2022-11-23 16:43:35 +0530

    2nd commit

---
+++ src/src/main/java/com/iris22a/config/TestBase.java
@@ -0,0 +1,42 @@
+package com.iris22a.config;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import com.iris22a.keyword.UIKeywords;
+import com.iris22a.utils.Environment;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class TestBase {
+	public static final Logger log = Logger.getLogger(TestBase.class);
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		String browserName = Environment.BROWSER;
+		if (browserName == null || browserName.isEmpty()) {
+			browserName = "Chrome";
+		}
+		if (browserName.isEmpty()) {
+			browserName = "Chrome";
+			log.info("\"Default browser is chrome\"");
+		}
+
+		UIKeywords.openBrowser(browserName);
+		log.info(browserName + " is open ");
+
+	}
+
+	@AfterMethod
+	public void tearDown() throws Exception {
+		UIKeywords.closeBrowser();
+
+	}
+
+}
